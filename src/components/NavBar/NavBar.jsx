4d036b617a598dbb949034f2a9128cd25998a936
Clarisse-Pimentel <clarilpim@gmail.com>
2026-05-27 21:43:36 -0300

    chore: ajustes na seção 1

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,24 +4,26 @@ import Logo from "../../assets/logo-plataforma.png";
 import Search from "../../assets/search.png"
 import Shop from "../../assets/shop.png";
 import User from "../../assets/user.png";
+import Menu from "../../assets/menu.png"
 
 const NavBar = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   
   return (
     <nav className={style.container}>
-        <img className={style.logo} src={Logo} alt="" />
-        <p className={style.title}>MINDEMY</p>
-          <div className={style.rightSide}>
-            <div className={style.content}>
-              <img className={style.search} src={Search} alt="" />
-              Pesquisar por qualquer coisa
-            </div>
-            <img className={style.shop} src={Shop} alt="" />
-            <button onClick={() => navigate("/login")}>
-              <img src={User} alt="" />
-            </button>
-          </div>
+      <img className={style.menu} src={Menu} />
+      <img className={style.logo} src={Logo} alt="" />
+      <p className={style.title}>MINDEMY</p>
+      <div className={style.rightSide}>
+        <div className={style.content}>
+          <img className={style.search} src={Search} alt="" />
+          Pesquisar por qualquer coisa
+        </div>
+        <img className={style.shop} src={Shop} alt="" />
+        <button onClick={() => navigate("/login")}>
+          <img src={User} alt="" />
+        </button>
+      </div>
     </nav>
   )
 }
